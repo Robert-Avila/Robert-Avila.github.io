2871ed7e806f3f8fdd274d99ebe458093e112d1a
Robert Avila <robertwavila@gmail.com>
2024-07-23 02:41:15 -0300

    upload login screen

---
+++ tela-de-login/src/components/Button/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Button = styled.button`
+  border-radius: 10px;
+  padding: 20px;
+  margin: 0;
+  outline: none;
+  border: none;
+  width: 100%;
+  cursor: pointer;
+  background-color: #01b421;
+  color: white;
+  font-weight: 900;
+  font-size: 20px;
+  max-width: 250px;
+`;
---
+++ tela-de-login/src/pages/Home/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100vh;
+  gap: 20px;
+  color: #f2f2f2;
+  font-family: "Montserrat", sans-serif;
+  font-size: 40px !important;
+  font-weight: 900;
+  line-height: 68px;
+  letter-spacing: 2px;
+  text-align: center;
+  text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3);
+`;
+
+export const Title = styled.h2``;
